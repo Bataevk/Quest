f389d9f1b8e3a1b36295c817bb14faeea7836eac
KilirrKH <108339911+KilirrKH@users.noreply.github.com>
2022-07-05 16:43:01 +0500

    Add files via upload

---
+++ consol_Quest/consol_Quest/Location.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consol_Quest
+{
+    internal class Location
+    {
+        public string name { get; private set; }
+        public string description { get; private set; }
+        public List<MyObject> invetory { get; private set; }
+
+        public Location(string name, string description, List<MyObject> invetory)
+        {
+            this.name = name;
+            this.description = description;
+            this.invetory = invetory;
+        }
+
+        public string GetDescription()
+        {
+            string secDes = "Оглядевшись вы заметили следующие предметы: ";
+            foreach (MyObject m in invetory)
+            {
+                secDes += m.description + ", ";
+            }
+            return description + secDes + "...";
+        }
+        public List<MyObject> GetObjects()
+        {
+            return invetory;
+        }
+        public string[] GetObjectName()
+        {
+            string[] names = new string[invetory.Count];
+            for (int i = 0; i < invetory.Count; i++)
+            {
+                names[i] = invetory[i].name;
+            }
+            return names;
+        }
+        public MyObject TakeObject(int index)
+        {
+            MyObject ob = invetory[index];
+            invetory.RemoveAt(index);
+            return ob;
+        }
+    }
+}
---
+++ consol_Quest/consol_Quest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consol_Quest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Вы готовы начать? Нажмити любую клавишу");
+            Console.ReadLine();
+            Console.WriteLine("Напишите ваше имя.");
+            string name = Convert.ToString(Console.ReadLine());
+            Game game = new Game(name);
+            game.Events(0);
+            Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Через некоторое время Вы приходите в себя и понимаете, что Вы в больнице.");
+            Console.WriteLine("Свет слепит Вас. Вы понимаете, что это был сон.");
+        }
+    }
+}
